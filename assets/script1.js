65f9acd04c6a8c1bb2882b845e71e47df09dbdfd
jcisne2 <cisnejos000@gmail.com>
2022-08-01 21:04:09 -0700

    quiz challenge

---
+++ assets/script1.js
@@ -0,0 +1,137 @@
+var questions = [
+    { 
+        question: "How many Infinity Stones are there?", 
+        answers: [
+            { text: "3", correct: false },
+            { text: "6" , correct: true },
+            { text: "5", correct: false },
+            { text: "10", correct: false }
+        ]
+    },
+    { 
+        question: "What is the only food that cannot go bad?", 
+        answers: [
+            { text: "Dark chocolate", correct: false },
+            { text: "Peanut butter", correct: false },
+            { text: "Honey", correct: true },
+            { text: "Canned tuna", correct: false }
+        ]
+    },
+    { 
+        question: "What is the most visited tourist attraction in the world?", 
+        answers: [
+            { text: "Eiffel Tower", correct: true },
+            { text: "Statue of Liberty", correct: false },
+            { text: "Great Wall of China", correct: false },
+            { text: "Colosseum", correct: false }
+        ]
+    },
+    { 
+        question: "What is the heaviest organ in the human body?", 
+        answers: [
+            { text: "Brain", correct: false },
+            { text: "Liver", correct: true },
+            { text: "Skin", correct: false },
+            { text: "Heart", correct: false }
+        ]
+    },
+    { 
+        question: "What element does the chemical symbol Au stand for?",
+        answers: [
+            { text: "Silver", correct: false },
+            { text: "Magnesium", correct: false },
+            { text: "Salt", correct: false },
+            { text: "Gold", correct: true }
+        ]
+    },
+    { 
+        question: "On average, how many seeds are located on the outside of a strawberry?",
+        answers: [
+            { text: "200", correct: true },
+            { text: "50", correct: false },
+            { text: "100", correct: false },
+            { text: "150", correct: false }
+        ]
+    },
+    { 
+        question: "What is the oldest soft drink in the United States?",
+        answers: [
+            { text: "Coca Cola", correct: false },
+            { text: "Pepsi", correct: false },
+            { text: "Dr. Pepper", correct: true },
+            { text: "Canada Dry Ginger Ale", correct: false }
+        ]
+    },
+    { 
+        question: "What river passes through New Orleans, Louisiana?",
+        answers: [
+            { text: "Orleans River", correct: false },
+            { text: "Mississippi River", correct: true },
+            { text: "Atchafalaya River", correct: false },
+            { text: "Colorado River", correct: false }
+        ]
+    },
+    { 
+        question: "What is the highest-grossing video game franchise to date?",
+        answers: [
+            { text: "Mario", correct: false },
+            { text: "Pokemon", correct: true },
+            { text: "Call of Duty", correct: false },
+            { text: "Street Fighter", correct: false }
+        ]
+    },
+    { 
+        question: "Which sea creature has three hearts?",
+        answers: [
+            { text: "Shark", correct: false },
+            { text: "Jellyfish", correct: false },
+            { text: "Stingray", correct: false },
+            { text: "Octopus", correct: true }
+        ]
+    },
+    { 
+        question: "What year was In-N-Out Burger founded?",
+        answers: [
+            { text: "1968", correct: false },
+            { text: "1975", correct: false },
+            { text: "1948", correct: true },
+            { text: "1985", correct: false }
+        ]
+    },
+    { 
+        question: "How many islands are in Hawaii?",
+        answers: [
+            { text: "32", correct: false },
+            { text: "94", correct: false },
+            { text: "156", correct: false },
+            { text: "137", correct: true }
+        ]
+    },
+    { 
+        question: "What is the national alcoholic beverage of America?",
+        answers: [
+            { text: "Bourbon", correct: true },
+            { text: "Rye", correct: false },
+            { text: "Whiskey", correct: false },
+            { text: "Cognac", correct: false }
+        ]
+    },
+    { 
+        question: "What was the very first Pixar movie?",
+        answers: [
+            { text: "Toy Story", correct: true },
+            { text: "A Bug's Life", correct: false },
+            { text: "Monsters, Inc.", correct: false },
+            { text: "Finding Nemo", correct: false }
+        ]
+    },
+    { 
+        question: "Which ocean is the Bermuda Triangle in?",
+        answers: [
+            { text: "Pacific Ocean", correct: false },
+            { text: "Caribbean Ocean", correct: false },
+            { text: "Indian Ocean", correct: false },
+            { text: "Atlantic Ocean", correct: true }
+        ]
+    },
+];
